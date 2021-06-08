a6611ca5bfe39ca820611e7d3151332438e159f5
blalic <blalic79@gmail.com>
2021-06-08 17:31:17 +0200

    table 2

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
+
+    public function item(){
+
+        return $this->belongsToMany(Item::class, 'item_menu');
+
+    }
 }
